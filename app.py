c59b820dadea050e0488ea04b4f0650e2b79a63f
Sergey Stetsiuk <serg2033@gmail.com>
2024-03-15 16:58:03 +0700

    some route

--- app.py
+++ app.py
@@ -5,8 +5,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello"
+    return "<h1>hello123</h1>"
+
+
+@app.route('/test')
+def test_page():
+    return "<h5>test page</h5>"
+
+
+@app.route('/greet/<name>')
+def name(name):
+    return f"<h3>hello {name}</h3>"
+
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    #number1 = int(number1)
+    #number2 = int(number2)
+    return f"{number1} + {number2} = {number1 + number2}"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5555, host="0.0.0.0")
